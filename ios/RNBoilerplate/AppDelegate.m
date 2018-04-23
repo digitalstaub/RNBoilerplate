1ca3f5b112f5fa4791e2f96abf6fd031010fd6b3
Benjamin <benni@digitalstaub.com>
2018-04-23 18:17:19 +0200

    linked i18n

--- ios/RNBoilerplate/AppDelegate.m
+++ ios/RNBoilerplate/AppDelegate.m
@@ -18,6 +18,7 @@
 {
   NSURL *jsCodeLocation;
 
+//  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
